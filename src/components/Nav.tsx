bf4576f1ea85d8dba909f267964134fd053a9280
Your Name <kboothe617@gmail.com>
2024-09-10 17:31:13 -0600

    Nav.tsx nav bar linking between pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,22 @@
+import { NavLink } from 'react-router-dom';
+
+// Navigation bar component to link between the pages
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className="navbar navbar-expand-lg navbar-dar bg-dark">
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/SavedCandidates">Potential Candidates</NavLink>
+          </li> 
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
