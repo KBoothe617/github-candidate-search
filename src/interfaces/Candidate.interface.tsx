1c41d2b3eac638b2889a06bb6d5a373183455eaf
Your Name <kboothe617@gmail.com>
2024-09-10 17:19:38 -0600

    candidate.interface.tsx created interface named candidate

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,15 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+// Candidate interface with the following properties: name, login, location, avatar_url, html_url, email, bio, company
+interface Candidate {
+    readonly name: string | null;
+    readonly login: string | null;
+    readonly location: string | null;
+    readonly avatar_url: string | null;
+    readonly html_url: string | null;
+    readonly email: string | null;
+    readonly bio: string | null;
+    readonly company: string | null;
+}
+
+export default Candidate;
