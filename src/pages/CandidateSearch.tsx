b9954d2c4c304f43ef271db2eefbd8d81daaacd9
Your Name <kboothe617@gmail.com>
2024-09-10 18:20:27 -0600

    CandidateSearch.tsx const state to hold current candidate

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,24 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MdEmail } from 'react-icons/md';
+
 
 const CandidateSearch = () => {
+  // const to hold current candidate
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
+    name: '',
+    login: '',
+    location: '',
+    avatar_url: '',
+    html_url: '',
+    email: '',
+    bio: '',
+    company: '',
+  });
+
+
   return <h1>CandidateSearch</h1>;
 };
 
